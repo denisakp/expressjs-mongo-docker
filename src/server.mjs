cfd20900d14ba6f59d9f2da99e08ff3028a84f48
Denis AKPAGNONITE <akpagnonited@gmail.com>
2024-04-06 00:56:22 +0000

    added application server

---
+++ src/server.mjs
@@ -0,0 +1,43 @@
+import express, {json} from 'express';
+import cors from 'cors';
+import compression from "compression";
+
+// project imports
+import applicationEnv from "./configurations/config.mjs";
+import routes from "./routes/index.mjs";
+import routeNotFoundMiddleware from "./middlewares/404-routes.middleware.mjs";
+import errorResponseMiddleware from "./middlewares/error-response.middleware.mjs";
+
+
+// express app
+const app = express();
+
+// express middlewares
+app.use(cors());
+app.use(express.json())
+app.use(compression());
+app.use(errorResponseMiddleware)
+
+// express routers
+app.use('/api', routes);
+
+// routes middleware
+app.use(routeNotFoundMiddleware);
+
+// express server
+const server = app.listen(applicationEnv.port, applicationEnv.host, () => {
+    console.info("server started on port http://%s:%d", applicationEnv.host, applicationEnv.port)
+});
+
+process.on("SIGTERM", () => {
+    console.info('closing express server !')
+    server.close()
+});
+
+process.on('uncaughtException', (error) => {
+    console.error({name: `uncaughtException - ${error.name} `, message: error.message,})
+});
+
+process.on('unhandledRejection', (reason) => {
+    console.error({name: `unhandledRejection - ${reason.name}`, message: reason.message,})
+});
