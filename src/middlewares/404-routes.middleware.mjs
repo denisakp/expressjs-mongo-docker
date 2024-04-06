25d5973f8ea8cdd4a91117076257d5368690a38e
Denis AKPAGNONITE <akpagnonited@gmail.com>
2024-04-06 00:54:07 +0000

    added middlewares

---
+++ src/middlewares/404-routes.middleware.mjs
@@ -0,0 +1,5 @@
+function routeNotFoundMiddleware (req, res) {
+    return res.status(404).send("🤔 sorry but, there's no handler for this route 😴");
+}
+
+export default  routeNotFoundMiddleware;
---
+++ src/middlewares/error-response.middleware.mjs
@@ -0,0 +1,6 @@
+
+function errorResponseMiddleware(error, req, res, next) {
+    return res.status(500).json({ message: error.message });
+}
+
+export default errorResponseMiddleware;
